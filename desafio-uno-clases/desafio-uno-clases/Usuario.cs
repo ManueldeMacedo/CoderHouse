a8a5a0271f553bea74cb8b0f25768af96e98f7df
ManueldeMacedo <75130351+ManueldeMacedo@users.noreply.github.com>
2022-11-05 19:03:40 -0300

    Add files via upload

---
+++ desafio-uno-clases/desafio-uno-clases/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_uno_clases
+{
+    internal class Usuario
+    {
+        int Id;
+        string Nombre, Apellido, NombreUsuario, Contrasena, Mail;
+
+    }
+}
